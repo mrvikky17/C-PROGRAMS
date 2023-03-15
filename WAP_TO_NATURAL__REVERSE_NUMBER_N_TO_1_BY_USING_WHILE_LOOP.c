589b4e10f9ef6c9a00f9b2663f41ac1233c12282
mrvikky17 <114817912+mrvikky17@users.noreply.github.com>
2023-03-15 22:37:08 +0530

    Add files via upload

---
+++ WAP_TO_NATURAL__REVERSE_NUMBER_N_TO_1_BY_USING_WHILE_LOOP.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+    int n,i;
+    i=1;
+    scanf("%d",&n);
+    while (n>=i)
+    {
+        printf("%d\n",n);
+        n-- ;
+    }
+    return 0;
+    
+}
---
+++ Write_a_C_program_to_print_all_alphabets_from_a_to_z._-_using_while_loop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    i=65;
+    n=90;
+    while (i<=n)
+    {
+        printf("%c\n",i);
+        i++;
+    }
+    return 0;
+}
