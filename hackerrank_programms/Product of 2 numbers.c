dbbb79d7e1182c5e1d9cbfe418d59a2f5d69b4a2
mrvikky17 <114817912+mrvikky17@users.noreply.github.com>
2023-05-18 22:06:43 +0530

    Create Product of 2 numbers.c

---
+++ hackerrank_programms/Product of 2 numbers.c
@@ -0,0 +1,15 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main() {
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    c=a*b;
+    printf("%d",c);
+    return 0;
+}
