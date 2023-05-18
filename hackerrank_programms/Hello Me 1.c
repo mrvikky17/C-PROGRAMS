b9d52546f99966c2e7053689f5c63af5831b3a2b
mrvikky17 <114817912+mrvikky17@users.noreply.github.com>
2023-05-18 22:09:29 +0530

    Create Hello Me 1.c

---
+++ hackerrank_programms/Hello Me 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char a[100];
+    scanf("%s",a);
+    printf("Welcome %s",a);
+    return 0;
+}
