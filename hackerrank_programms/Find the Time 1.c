3be0063f64aa1736ae37ff4f68970b34d8620f0c
mrvikky17 <114817912+mrvikky17@users.noreply.github.com>
2023-05-18 22:08:56 +0530

    Create Find the Time 1.c

---
+++ hackerrank_programms/Find the Time 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    float t,r;
+    scanf("%f",&r);
+    t = (1/r);
+    printf("%.2f",t*100);
+    
+    
+    return 0;
+}
