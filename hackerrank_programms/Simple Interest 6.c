2434adb6fe8b759585b47553ce920215128b0773
mrvikky17 <114817912+mrvikky17@users.noreply.github.com>
2023-05-18 22:11:42 +0530

    Create Simple Interest 6.c

---
+++ hackerrank_programms/Simple Interest 6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int s, p,r,t,a;
+    scanf("%d",&p);
+    scanf("%d",&r);
+    scanf("%d",&t);
+    s=p*r*t*0.01;
+    a= s+p;
+    printf("%d",a);
+    
+    
+    
+    
+    return 0;
+}
