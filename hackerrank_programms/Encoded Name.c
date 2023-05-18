56f4f17454e79bcccdfc83f6e6328f558c6e0ce0
mrvikky17 <114817912+mrvikky17@users.noreply.github.com>
2023-05-18 22:12:49 +0530

    Create Encoded Name.c

---
+++ hackerrank_programms/Encoded Name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int a,b,c,d,e;
+    scanf("%d&%d&%d&%d&%d",&a,&b,&c,&d,&e);
+    printf("%c%c%c%c%c",a,b,c,d,e);
+    return 0;
+}
